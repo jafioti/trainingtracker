de5376c137781c201685135e579ba8b3a2ffebc9
Joe Fioti <jafioti@gmail.com>
2020-09-30 11:36:47 -0400

    Revert "Changed structure"
    This reverts commit 8d437d8fd1b500f7d50619a5a61e476e731c34cf.

--- trainingtracker/__init__.py
+++ trainingtracker/__init__.py
@@ -2,47 +2,40 @@ from trainingtracker import training_tracker_server
 import socket
 # TrainingTracker Main Script
 
-def start_server():
-    internal_state = {}
+internal_state = {}
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    ip = s.getsockname()[0]
-    print("TrainingTracker IP Address: " + str(ip))
-    s.close()
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+ip = s.getsockname()[0]
+print("TrainingTracker IP Address: " + str(ip))
+s.close()
 
-    queue = training_tracker_server.start_main_server()
-    return TTServer(queue, internal_state)
+queue = training_tracker_server.start_server()
 
-class TTServer:
-    def __init__(self, queue, internal_state):
-        self.queue = queue
-        self.internal_state = internal_state
-    
-    def add_scalar(self, tag, value, step):
-        '''Add scalar value.\n
-        tag (String): The category tag for the value, such as 'loss'
-        value (float): The actual value, the "Y-Axis"
-        step (int): The global step of the value, the "X-Axis"
-        '''
-        # Adjust the internal state
-        if tag in self.internal_state.keys():
-            self.internal_state[tag] += [{"y":value, "x":step}]
-        else:
-            self.internal_state[tag] = [{"y":value, "x":step}]
-        # Update the server process
-        self.queue.put_nowait(self.internal_state)
+def add_scalar(tag, value, step):
+    '''Add scalar value.\n
+    tag (String): The category tag for the value, such as 'loss'
+    value (float): The actual value, the "Y-Axis"
+    step (int): The global step of the value, the "X-Axis"
+    '''
+    # Adjust the internal state
+    if tag in internal_state.keys():
+        internal_state[tag] += [{"y":value, "x":step}]
+    else:
+        internal_state[tag] = [{"y":value, "x":step}]
+    # Update the server process
+    queue.put_nowait(internal_state)
 
-    def add_scalar_list(self, tag, values, steps):
-        '''Add a list of scalar values.\n
-        tag (String): The category tag for the value, such as 'loss'
-        values (float): The list of actual values, the "Y-Axis"
-        steps (int): The list of global steps of the values, the "X-Axis"
-        '''
-        # Adjust the internal state
-        if tag in self.internal_state.keys():
-            self.internal_state[tag] += [{"y":values[i], "x":steps[i]} for i in range(len(values))]
-        else:
-            self.internal_state[tag] = [{"y":values[i], "x":steps[i]} for i in range(len(values))]
-        # Update the server process
-        self.queue.put_nowait(self.internal_state)
+def add_scalar_list(tag, values, steps):
+    '''Add a list of scalar values.\n
+    tag (String): The category tag for the value, such as 'loss'
+    values (float): The list of actual values, the "Y-Axis"
+    steps (int): The list of global steps of the values, the "X-Axis"
+    '''
+    # Adjust the internal state
+    if tag in internal_state.keys():
+        internal_state[tag] += [{"y":value[i], "x":step[i]} for i in range(len(values))]
+    else:
+        internal_state[tag] = [{"y":value[i], "x":step[i]} for i in range(len(values))]
+    # Update the server process
+    queue.put_nowait(internal_state)
--- trainingtracker/training_tracker_server.py
+++ trainingtracker/training_tracker_server.py
@@ -1,12 +1,10 @@
 import asyncio, websockets, socket, multiprocessing, copy, time
 
 def run_server(queue):
-    print("Running serrver")
     async def send_message(websocket, path):
         while True:
             try:
                 incoming = await websocket.recv()
-                print("got")
 
                 # Get the latest internal state
                 while not queue.empty(): server_state = queue.get_nowait()
@@ -21,7 +19,7 @@ def run_server(queue):
     asyncio.get_event_loop().run_until_complete(server_start)
     asyncio.get_event_loop().run_forever()
 
-def start_main_server():
+def start_server():
     queue = multiprocessing.Queue()
     job = multiprocessing.Process(target=run_server, args=(queue,))
     job.start()
